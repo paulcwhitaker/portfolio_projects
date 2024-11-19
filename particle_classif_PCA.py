ec22dfeac429e09c054b3eef2434ef277430656f
paulcwhitaker <88857588+paulcwhitaker@users.noreply.github.com>
2024-11-19 03:09:16 -0500

    updated to include SVM classification

--- particle_classif_PCA.py
+++ particle_classif_PCA.py
@@ -62,3 +62,94 @@ plt.title('Task 6: Cumulative Information percentages')
 plt.xlabel('Principal Axes')
 plt.ylabel('Cumulative Proportion of Variance Explained')
 plt.show()
+
+
+# Standardize the data matrix
+mean = data_matrix.mean(axis=0)
+sttd = data_matrix.std(axis=0)
+data_matrix_standardized = (data_matrix - mean) / sttd
+
+
+
+# Find the principal components
+pca = PCA()
+
+# Fit the standardized data and calculate the principal components
+principal_components = pca.fit_transform(data_matrix_standardized)
+print(f'Number of features in the data matrix: {principal_components.shape[1]}')
+print(f'Number of features in the principal components: {data_matrix.shape[1]}')
+
+
+# Find the eigenvalues from the singular values
+singular_values = pca.singular_values_
+eigenvalues = singular_values ** 2
+
+
+#transpose components to get column forms
+eigenvectors = pca.components_.T
+
+# Get the variance ratios from the `explained_variance_ratio_`
+principal_axes_variance_ratios = pca.explained_variance_ratio_
+principal_axes_variance_percents = principal_axes_variance_ratios *100
+
+
+
+# Initialize a PCA object with 2 components
+pca = PCA(n_components=2) 
+ 
+# Fit the standardized data 
+principal_components = pca.fit_transform(data_matrix_standardized)
+ 
+
+print(f'Number of Principal Components Features: {principal_components.shape[1]}')
+print(f'Number of Original Data Features: {data_matrix_standardized.shape[1]}')
+
+
+# Plot the principal components 
+principal_components_data = pd.DataFrame({
+    'PC1': principal_components[:, 0],
+    'PC2': principal_components[:, 1],
+    'class': classes,
+})
+
+sns.lmplot(x='PC1', y='PC2', data=principal_components_data, hue='class', fit_reg=False)
+plt.show()
+
+
+# one-hot encoding
+y = classes.astype('category').cat.codes
+ 
+
+# Get principal components with 2 features 
+# Perform PCA using 2 components
+pca_1 = PCA(n_components=2) 
+ 
+# Use the principal components as X and split the data into 33% testing and the rest training
+X = pca_1.fit_transform(data_matrix_standardized) 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+ 
+# Create a Linear Support Vector Classifier
+svc_1 = LinearSVC(random_state=0, tol=1e-5)
+svc_1.fit(X_train, y_train) 
+ 
+# Generate a score for the testing data
+score_1 = svc_1.score(X_test, y_test)
+print(f'Score for model with 2 PCA features: {score_1}')
+
+
+# Using the original features 
+# Select two features from the original data
+first_two_original_features = [0, 1]
+X_original = data_matrix_standardized.iloc[:, first_two_original_features]
+ 
+# Split the data intro 33% testing and the rest training
+X_train, X_test, y_train, y_test = train_test_split(X_original, y, test_size=0.33, random_state=42)
+ 
+# Create a Linear Support Vector Classifier
+svc_2 = LinearSVC(random_state=0)
+svc_2.fit(X_train, y_train)
+ 
+# Generate a score for the testing data
+score_2 = svc_2.score(X_test, y_test)
+print(f'Score for model with 2 original features: {score_2}')
+
